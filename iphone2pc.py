0f66dc1371af625f37ccf250b610c8c81ffdeba7
PC <pavel_khudzik@epam.com>
2020-02-11 19:47:16 +0300

    last changes

--- iphone2pc.py
+++ iphone2pc.py
@@ -1,32 +1,41 @@
-import sys
-from pyicloud import PyiCloudService
+from icloudpd.authentication import authenticate, TwoStepAuthRequiredError
+from icloudpd import download
+from icloudpd.email_notifications import send_2sa_notification
+from icloudpd.string_helpers import truncate_middle
+from icloudpd.autodelete import autodelete_photos
+from icloudpd.paths import local_download_path
+from icloudpd import exif_datetime
+# Must import the constants object so that we can mock values in tests.
+from icloudpd import constants
+import datetime
 
 
-api = PyiCloudService('astremskaya@mail.ru', 'Julia120786')
 
-if api.requires_2fa:
-    import click
-    print("Two-step authentication required. Your trusted devices are:")
+try:
+    icloud = authenticate(
+        'astremskaya@mail.ru',
+        'Julia120786'
+    )
 
-    devices = api.trusted_devices
-    for i, device in enumerate(devices):
-        print("  %s: %s" % (i, device.get('deviceName', "SMS to %s" % device.get('phoneNumber'))))
+except TwoStepAuthRequiredError:
+    print('ERROR!')
+    exit(1)
 
-    device = click.prompt('Which device would you like to use?', default=0)
-    device = devices[device]
-    if not api.send_verification_code(device):
-        print("Failed to send verification code")
-        sys.exit(1)
+# Default album is "All Photos", so this is the same as
+# calling `icloud.photos.all`.
+albums = icloud.photos.albums
+print(albums)
 
-    code = click.prompt('Please enter validation code')
-    if not api.validate_verification_code(device, code):
-        print("Failed to verify verification code")
-        sys.exit(1)
+photos = icloud.photos.albums['All Photos']
+print(len(photos))
 
-print(api.photos.albums.get())
+for photo in photos:
+    if photo.created.strftime('%Y%m') in ('201803','201804','201805','201806'):
+        photo_created = photo.created.strftime('%Y%m%d')
+        photo_added = photo.added_date.strftime('%Y%m%d')
+        print(photo, photo.filename, photo.item_type, photo_created, photo_added)
+        download = photo.download()
+        with open('d:\\iPhone_photos_2018\\' + photo_created + '_' + photo.filename, 'wb') as opened_file:
+            opened_file.write(download.raw.read())
 
-
-#for photo in api.photos.all:
-#    print(photo.filename, photo.created, sep=' - ')
-
-
+print('~~~ DONE ~~~')
